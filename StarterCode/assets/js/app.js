86aaadec8d4096cc0c9eaf619ea959dfa4cbc293
Preeti Shukla <preetism80@gmail.com>
2018-12-30 22:57:40 -0800

    Final

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -1,143 +1,120 @@
-d3.select(window).on("resize", handleResize);
 
-// When the browser loads, call loadchart()
-loadChart();
+var svgWidth = 900;
+var svgHeight = 700;
 
-function handleResize() {
-  var svgArea = d3.select("svg");
-
-  // If there is already an svg container on the page, remove it and reload the chart
-  if (!svgArea.empty()) {
-    svgArea.remove();
-    loadChart();
-  };
-};
-
-function loadChart() {
-    var svgWidth = window.innerWidth;
-    var svgHeight = window.innerHeight;
-
-var svgWidth = 960;
-var svgHeight = 500;
-
+//adjust margins here (must be large enough to keep axis labels outside of chart)
 var margin = {
-  top: 20,
-  right: 40,
-  bottom: 60,
-  left: 100
+  top: 30,
+  right: 20,
+  bottom: 70,
+  left: 110,
 };
 
+//chart area
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
-// Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
-var svg = d3.select(".chart")
-  .append("svg")
-  .attr("width", svgWidth)
-  .attr("height", svgHeight)
 
-var chartGroup = svg.append("g")
+// Create an SVG wrapper, append an SVG group that will hold our scatter plot, and shift the latter by left and top margins.
+var svg = d3.select("#scatter")
+.append("svg")
+.attr("width", svgWidth)
+.attr("height", svgHeight);
+
+// Append an SVG group
+var scatter_chart = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-// Import Data
-d3.csv("/data/cleaneddata.csv", function (error, demoData) {
-    if (error)throw error;
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// Import data from the CSV file and execute everything below
+d3.csv("assets/data/data.csv", function(err, data) {
+  if (err) throw err;
+ 
+ // Step 1: Parse Data
+ data.forEach(function(d) {
+    d.income = +d.income;
+    d.smokes= +d.smokes;  
+  });
+/////////////////////////////////////////////////////////////////////////////
+// Step 2: Create scale functions
+// xLinearScale function above csv import
 
-  //Parse Data/Cast as numbers
-  demoData.forEach(function (data) {
-    data.poverty = +data.poverty;
-    data.healthcare = +data.healthcare;
+var xLinearScale = d3.scaleLinear()
+  .domain([d3.min(data, d => d.income), d3.max(data, d => d.income)])
+  .range([0, width]);
+
+// Create y scale function
+var yLinearScale = d3.scaleLinear()
+  .domain([d3.min(data, d => d.smokes), d3.max(data, d => d.smokes)+1])
+  .range([height, 0]);
+  
+// Create axis functions
+// Create initial axis functions
+var bottomAxis = d3.axisBottom(xLinearScale);
+var leftAxis = d3.axisLeft(yLinearScale);
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Append axes to the chart (see 03-example12)
+scatter_chart.append("g")
+  .attr("transform", `translate(0, ${height})`)
+  .call(bottomAxis);
+
+// append y axis
+scatter_chart.append("g")
+  .call(leftAxis);
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Create Circles.  
+var circlesGroup = scatter_chart.selectAll("circle").data(data).enter(); 
+
+//Separate append and attributes and use d3-tip.js
+var d3Tip=circlesGroup
+  .append("circle")  
+  .classed("stateCircle", true)
+  .attr("cx", d => xLinearScale(d.income))
+  .attr("cy", d => yLinearScale(d.smokes))
+  .attr("r", "15")
+  .attr("opacity", ".2");
+  
+//Create text labels with state abbreviation for each circle
+circlesGroup.append("text")
+  .classed("stateText", true)
+  .attr("x", d => xLinearScale(d.income))
+  .attr("y", d => yLinearScale(d.smokes))
+  .attr("stroke", "purple")
+  .attr("font-size", "10px")
+  .text(d => d.abbr)
+    
+
+var toolTip = d3.tip()
+    .attr("class", "d3-tip")  //used to get correct pop up
+    .offset([-10, 0])
+    .html(function(d) {
+      return (`${d.state}<br>Median HH income: $${d.income}<br>Smokes: ${d.smokes}%`);
   });
 
-  //Create scale functions
-  var xLinearScale = d3.scaleLinear()
-    .domain(d3.extent(demoData, d => d.poverty))
-    .range([0, width]);
+//Create tooltip in the chart
 
-  var yLinearScale = d3.scaleLinear()
-    .domain([55, d3.max(demoData, d => d.healthcare + 5)])
-    .range([height, 0]);
+d3Tip.call(toolTip);
 
-  //Create axis functions
-  var bottomAxis = d3.axisBottom(xLinearScale);
-  var leftAxis = d3.axisLeft(yLinearScale);
+d3Tip.on("mouseout", function(d, index) {
+    d3.select(this).style("stroke","red")
+    .attr("r", "15")
+    toolTip.show(d);
+  });
 
-  //Append Axes to the chart
-  chartGroup.append("g")
-    .attr("transform", `translate(0, ${height})`)
-    .call(bottomAxis);
+// Create X-axis label
+scatter_chart.append("text")
+.attr("transform", "rotate(-90)")
+.attr("y", 40 - margin.left)
+.attr("x", -100 - (height / 2))
+.attr("dy", "1em")
+.attr("class", "axisText")
+.text("Percent population smokes(%)");
 
-  chartGroup.append("g")
-    .call(leftAxis);
-
-   //Create Circles
-  var circlesGroup = chartGroup.selectAll("circle")
-  .data(demoData)
-  .enter()
-  .append("circle")
-  .attr("cx", d => xLinearScale(d.poverty))
-  .attr("cy", d => yLinearScale(d.healthcare))
-  .attr("r", "12")
-  .attr("fill", "blue")
-  .attr("opacity", ".5")
-
-  //Create state labels
-  labelGroup = chartGroup.append("text")
-        .style("text-anchor", "middle")
-        .style("font-size", "10px")
-        .selectAll("tspan")
-        .data(demoData)
-        .enter()
-        .append("tspan")
-        .attr("x", function(data) {
-          return xLinearScale(data.poverty - 0);
-        })
-        .attr("y", function(data) {
-          return yLinearScale(data.healthcare - 0.2);
-        })
-        .text(function(data) {
-          return data.stateCode
-        });
-  
-
-  //Initialize tool tip
-  var toolTip = d3.tip()
-    .attr("class", "tooltip")
-    .offset([80, -60])
-    .html(function (d) {
-      return (`${d.state}<br>Poverty: ${d.poverty}<br>healthcare: ${d.healthcare}`);
-    });
-
-  //Create tooltip in the chart
-  chartGroup.call(toolTip);
-
-  //Create event listeners to display and hide the tooltip
-  circlesGroup.on("click", function (data) {
-      toolTip.show(data);
-    })
-  
-  labelGroup.on("click", function(data){
-      toolTip.show(data);
-  })
-
-    //onmouseout event
-    .on("mouseout", function (data, index) {
-      toolTip.hide(data);
-    });
-
-  // Create axes labels
-  chartGroup.append("text")
-    .attr("transform", "rotate(-90)")
-    .attr("y", 0 - margin.left + 40)
-    .attr("x", 0 - (height / 2))
-    .attr("dy", "1em")
-    .attr("class", "axisText")
-    .text("Healthcare(%)");
-
-  chartGroup.append("text")
-    .attr("transform", `translate(${width/2}, ${height + margin.top + 30})`)
-    .attr("class", "axisText")
-    .text("Poverty(%)");
-});
-
-};
+// Create Y-axis label
+scatter_chart.append("text")
+.attr("transform", `translate(${width/3}, ${height + margin.top + 30})`)
+.attr("class", "axisText")
+.text("Median Household Income($)");
+    
+}); 
